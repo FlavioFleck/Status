08d520b70716576aa9deed89161ba95778378c0c
PedroXits <pedroo.asn@gmail.com>
2025-11-05 22:13:39 -0300

    Criação do authMiddleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,6 +2,21 @@ import { verifyToken } from "../utils/jwt.js";
 
 export function authMiddleware(req, res, next) {
     const authHeader = req.headers.authorization;
-    
+    if(!authHeader) {
+        return res.status(401).send({
+            error: "Token não fornecido!"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch {
+        return res.status(401).send({
+            error: "Token inválido ou expirado."
+        });
+    }
 }
 
