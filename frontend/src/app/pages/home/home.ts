ce1383c03e6682b7822e20dcb56b5ed938caacf7
PedroXits <pedroo.asn@gmail.com>
2025-11-08 17:14:54 -0300

    Adição animção na HOME

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -7,8 +7,21 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'] 
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active');
+          obs.unobserve(entry.target); 
+        }
+      });
+    }, { threshold: 0.2 }); 
+
+    document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
+  }
 
 }
