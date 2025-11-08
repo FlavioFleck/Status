abf1c831ca07d81f1c5bf17a8fb3cf16f48b355c
PedroXits <pedroo.asn@gmail.com>
2025-11-08 18:14:35 -0300

    Criação do login.html + login.css

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout';
 import { BookingComponent } from './pages/booking/booking';
 import { HomeComponent } from './pages/home/home';
+import { LoginComponent } from './pages/login/login';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,8 @@ export const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', component: HomeComponent },
-            { path: 'booking', component: BookingComponent }
+            { path: 'booking', component: BookingComponent },
+            { path: 'login', component: LoginComponent }
         ]
     }
 ];
