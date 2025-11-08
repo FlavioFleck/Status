f94758a7d808a702cd79472f009cc841c03679ea
PedroXits <pedroo.asn@gmail.com>
2025-11-08 00:21:31 -0300

    Adição das rotas do auth

---
+++ backend/src/routes/authRouter.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const router = express.Router()
+
+import connection from '../connection.js'
+import AuthController from '../controllers/AuthController.js'
+
+const authController = new AuthController(connection);
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -2,12 +2,13 @@ import express from 'express'
 const router = express.Router()
 
 import userRouter from '../routes/userRouter.js'
+import authRouter from '../routes/authRouter.js'
 import appointmentRouter from '../routes/appointmentRouter.js'
 import productRouter from "../routes/productRouter.js"
 
 router.use("/user", userRouter)
+router.use("/auth", authRouter)
 router.use('/appointment', appointmentRouter)
 router.use("/product", productRouter)
-
 
 export default router
