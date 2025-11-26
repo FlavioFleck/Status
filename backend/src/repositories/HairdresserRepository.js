d2a989478c8c31b3c42a439ba689421d5fe5e041
PedroXits <pedroo.asn@gmail.com>
2025-11-26 15:35:15 -0300

    Criação do add() no UserRepository

--- backend/src/repositories/HairdresserRepository.js
+++ backend/src/repositories/HairdresserRepository.js
@@ -2,4 +2,20 @@ export default class HairdresserRepository {
     constructor(connection) {
         this.connection = connection;
     }
+
+    async add({name, cpf, availability}) {
+        const query =`
+            INSERT INTO hairdressers(name, cpf, availability)
+                VALUES(?, ?, ?); 
+        `
+
+        const [info] = await this.connection.query(query, [
+            name,
+            cpf,
+            availability
+        ]);
+        return info;
+    }
+
+    
 }
