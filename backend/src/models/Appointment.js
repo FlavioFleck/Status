2a4ec80a804f11b99313257796bdc87986f91354
PedroXits <pedroo.asn@gmail.com>
2025-11-26 18:09:47 -0300

    Adição do CRUD completo ao AppointmentRepository

--- backend/src/models/Appointment.js
+++ backend/src/models/Appointment.js
@@ -1,9 +1,8 @@
 export default class Appointment {
-    constructor({user_id, hairdresser_id, service_id, appointment_id, appointment_datetime, status}) {
+    constructor({user_id, hairdresser_id, service_id, appointment_datetime, status}) {
         this.user_id = user_id
         this.hairdresser_id = hairdresser_id
         this.service_id = service_id
-        this.appointment_id = appointment_id
         this.appointment_datetime = appointment_datetime
         this.status = status
     }
--- backend/src/repositories/AppointmentRepository.js
+++ backend/src/repositories/AppointmentRepository.js
@@ -1,15 +1,82 @@
 export default class AppointmentRespository {
     constructor(connection) {
-        this.connection
+        this.connection = connection;
     }
 
-    add(){}
+    async add({user_id, hairdresser_id, service_id, appointment_datetime, status}){
+        const query = `
+            INSERT INTO appointments(user_id, hairdresser_id, service_id, appointment_datetime, status)
+                VALUES (?, ?, ?, ?, ?);
+        `
 
-    delete(){}
+        const [info] = await this.connection.query(query,[
+            user_id,
+            hairdresser_id,
+            service_id,
+            appointment_datetime,
+            status
+        ]);
+        return info;
+    }
+
+    async delete({id}){
+        const query = `
+            DELETE FROM appointments
+                WHERE id = ?;
+        `
+
+        const [info] = await this.connection.query(query, [
+            id
+        ]);
+        return info.affectedRows > 0;
+    }
     
-    update(){}
+    async update({id, data}){
+        const query = `
+            UPDATE appointments
+                SET user_id = ?,
+                    haidresser_id = ?,
+                    service_id = ?,
+                    status = ?
+                WHERE id = ?;
+        `
+        const [info] = await this.connection.query(query, [
+            id,
+            data
+        ]);
+        return info;
+    }
 
-    get(){}
+    async getAll(){
+        const query = `
+            SELECT  user_id,
+                    hairdresser_id,
+                    service_id,
+                    appointment_datetime,
+                    status,
+                    created_at
+                FROM appointments;
+        `
 
-    getAll(){}
+        const [info] = await this.connection.query(query);
+        return info;
+    }
+
+    async getById({id}){
+        const query = `
+            SELECT  user_id,
+                    hairdresser_id,
+                    service_id,
+                    appointment_datetime,
+                    status,
+                    created_at
+                FROM appointments 
+            WHERE id = ?;
+        `
+
+        const [info] = await this.connection.query(query, [
+            id
+        ]);
+        return info[0];
+    }
 }
