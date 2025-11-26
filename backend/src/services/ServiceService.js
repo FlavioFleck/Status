e76a08d5f9a98af9eabb46f3de812ac9b99d9149
PedroXits <pedroo.asn@gmail.com>
2025-11-26 20:28:24 -0300

    Criando o arquivo ServiceService

---
+++ backend/src/services/ServiceService.js
@@ -0,0 +1,8 @@
+import Service from "../models/Service.js";
+
+export default class ServiceService {
+    constructor(serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+    
+}
