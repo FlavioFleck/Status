1c2b486b9c324ed90dfbd4704f3b967e99a0716f
PedroXits <pedroo.asn@gmail.com>
2025-11-26 22:55:50 -0300

    Criação do model AvailableTimes

---
+++ backend/src/models/AvailableTimes.js
@@ -0,0 +1,8 @@
+export default class AvailableTimes {
+    constructor(hairdresser_id, date, time, is_booked) {
+        this.hairdresser_id = hairdresser_id
+        this.date = date,
+        this.time = time,
+        this.is_booked = is_booked
+    }
+}
