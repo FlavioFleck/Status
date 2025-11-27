e38d686030b9c4841ec8e6c015173dc0f7592bd1
PedroXits <pedroo.asn@gmail.com>
2025-11-26 22:24:49 -0300

    Adição do CRUD no AppointmentService

--- backend/src/services/AppointmentService.js
+++ backend/src/services/AppointmentService.js
@@ -1,5 +1,45 @@
+import Appointment from "../models/Appointment.js";
+
 export default class AppointmentService {
     constructor(appointmentRespository) {
         this.appointmentRespository = appointmentRespository
+    }
+
+    async createAppointment(payload) {
+        const appointment = new Appointment(payload);
+        const result = await this.appointmentRespository.add(appointment);
+        return result;
+    }
+
+    async deleteAppointment(payload) {
+        const success = await this.appointmentRespository.delete(payload);
+        if(!success){
+            throw new Error("Agendamento informado não encontrado ou inexistente.")
+        }
+        return success;
+    }
+
+    async updateAppointment(payload) {
+        const result = await this.appointmentRespository.update(payload);
+        if(!result) {
+            throw new Error("Erro ao atualizar os dados do agendamento.");
+        }
+        return result;
+    }
+
+    async getAllAppointments() {
+        const result = await this.appointmentRespository.getAllAppointments();
+        if(!result) {
+            throw new Error("Nenhum agendamento encontrado.")
+        }
+        return result;
+    }
+
+    async getAppointmentById() {
+        const result = await this.appointmentRespository.getAppointmentById(payload);
+        if(!result){
+            throw new Error("O agendamento com o id informado não existe.");
+        }
+        return result;
     }
 }
