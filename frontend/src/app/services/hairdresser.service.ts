1b8408eb7cfbe356838d89f82b68733b3b8792ee
PedroXits <pedroo.asn@gmail.com>
2025-11-27 02:53:02 -0300

    Adicionando as funções no hairdresser.service.ts

--- frontend/src/app/services/hairdresser.service.ts
+++ frontend/src/app/services/hairdresser.service.ts
@@ -5,8 +5,24 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  private API_URL = 'http://localhost:5010/hairdresser';
+export class HairdresserService {
+    private API_URL = 'http://localhost:5010/hairdresser';
+    
+    constructor(private http: HttpClient) {
+
+    }
+
+    create(data: any) {
+        return this.http.post(`${this.API_URL}/create`, data);
+    }
+
+    delete(data: any) {
+        return this.http.delete(`${this.API_URL}/delete/${data}`);
+    }
+
+    getAll() {
+        return this.http.get(`${this.API_URL}/getAll`);
+    }
   
 
 }
