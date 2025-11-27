a0fc5176e8be0b2e7acd9c22aaa9f12f7362c2f5
wkarina <karii46y@gmail.com>
2025-11-27 01:21:38 -0300

    botão de agendar no /services + implementação de mock

--- frontend/src/app/pages/price-list/price-list.ts
+++ frontend/src/app/pages/price-list/price-list.ts
@@ -13,14 +13,38 @@ export class PriceListComponent {
 
   priceList = [
     {
-      category: 'Cabelos',
+      category: 'Cortes',
       services: [
-        { name: 'Corte Feminino', price: 'R$ 80,00' },
-        { name: 'Escova', price: 'R$ 40,00' },
-        { name: 'Escova Modelada', price: 'R$ 60,00' },
-        { name: 'Hidratação', price: 'R$ 80,00' }
+        { name: 'Corte Masculino', price: 'R$ 80,00' },
+        { name: 'Corte Feminino', price: 'R$ 80,00' }
+      ]
+    },
+    {
+      category: 'Escovas',
+      services: [
+        { name: 'Escova Simples', price: 'R$ 40,00' },
+        { name: 'Escova Modelada', price: 'R$ 60,00' }
+      ]
+    },
+    {
+      category: 'Química / Coloração',
+      services: [
+        { name: 'Aplicação de Tinta', price: 'R$ 70,00' },
+        { name: 'Tintura', price: 'R$ 100,00' },
+        { name: 'Progressiva', price: 'R$ 150,00' },
+        { name: 'Luzes', price: 'R$ 250,00' }
+      ]
+    },
+    {
+      category: 'Depilação',
+      services: [
+        { name: 'Buço', price: 'R$ 25,00' },
+        { name: 'Axila', price: 'R$ 40,00' },
+        { name: '1/2 Perna', price: 'R$ 50,00' },
+        { name: 'Perna Inteira', price: 'R$ 70,00' },
+        { name: 'Virilha Cavada', price: 'R$ 50,00' },
+        { name: 'Virilha Completa', price: 'R$ 60,00' }
       ]
     },
   ];
-
 }
