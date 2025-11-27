6fd628495ddc7697d4f2cbe0e27cc72a3885353b
PedroXits <pedroo.asn@gmail.com>
2025-11-27 02:36:10 -0300

    Criando o arquivo hairdresser.service.ts

---
+++ frontend/src/app/services/hairdresser.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private API_URL = 'http://localhost:5010/hairdresser';
+  
+
+}
